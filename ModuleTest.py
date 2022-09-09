3a3b1ac6cdd0f145d5c16f93e480e852ef39071a
Hongyu Liu <hongyul9@uci.edu>
2022-09-08 21:52:42 -0700

    Added Module Test and README

--- ModuleTest.py
+++ ModuleTest.py
@@ -1,15 +1,65 @@
 ####################
 ## Module Testing ##
 
+import MiningBot_Utilities
 import pyautogui as pt
-import time
+from time import sleep
+import keyboard
+
+def _moveCharacter(key_press, duration, action = 'walking'):
+    if action == 'walking':
+        pt.keyDown(key_press)
+        sleep(duration)
+        pt.keyUp(key_press)
+
+        print("Moved the character for {} seconds.".format(str(duration)))
+
+    
+    elif action == 'dashing':
+        pt.keyDown(MiningBot_Utilities.DASH_KEY)
+        pt.keyDown(key_press)
+        sleep(duration)
+        pt.keyUp(key_press)
+        pt.keyUp(MiningBot_Utilities.DASH_KEY)
+
+        print("Moved the character for {} seconds.".format(str(duration)))
+    
+    return 0
+
+def testKeyboardCtrl():
+    print("---------------Keyboard contral test starts:---------------")
+    input("1. Please open a text editor (Notebook, GoogleDocs, Word, etc.). When you are done, go back to the terminal press ENTER to continue.")
+    print("2. Please left click on the input line so that your text editor is ready to accept inputs. Leave the cursor there and press SHIFT directly.")
+    finished = False
+    while not finished:
+        if keyboard.is_pressed("shift"):
+            cursor_position = pt.position()
+            print("SHIFT pressed! Cursor positions recorded: {}.".format(cursor_position))
+            finished = True
+    
+    input("3. Feel free to move the cursor around. Go back to the terminal and Press ENTER. You should see the following line being typed in your text editor:")
+    pt.moveTo(cursor_position)
+    pt.leftClick(duration= 0.2)
+    pt.write("#This is typed by the bot!!")
+    print("  #This is typed by the bot!! Congratulations if you see this!")
+    print("4. Please check system preference if the test is NOT passed and restart the test.")
+    print("---------------Keyboard contral test finished.---------------")
+
+def testMinecraftMouse():
+
+    sleep(3)
+    _moveCharacter('w', 2, action= 'dashing')
+    _moveCharacter('num4', MiningBot_Utilities.TURN_TIME)
+    _moveCharacter('w', 2)
+
+
 
 
 def main():
     print(pt.size())
 
     print(pt.position())
-    time.sleep(1)
+    sleep(1)
 
     initial_pos = pt.position()
     print(initial_pos)
@@ -26,7 +76,7 @@ def main():
     #.click(X, Y, number_of_clicks, duration, button = "left")
     # other clicks include .doubleClick, .leftClick, .rightClick. etc.
 
-    pt.click(234, 372, 1 , 0.2, button= "left")
+    # pt.click(234, 372, 1 , 0.2, button= "left")
 
     # .mouseDown(X, Y, button =) + .mouseUp(X, Y, button =) hold down the button and "drag"
     # pt.mouseDown(100, 100, button = "left")
@@ -43,34 +93,34 @@ def main():
 
     ## Keyboard functions
     # type keyboard
-    # pt.moveTo(initial_pos)
-    # pt.leftClick(duration= 0.2)
-    # pt.write("#This is typed by the bot!!")
-    # pt.press("enter")
-    # pt.write("#Another line typed by the bot!!")
-    # This is typed by the bot!!
-    # Another line typed by the bot!!
+    pt.moveTo(initial_pos)
+    pt.leftClick(duration= 0.2)
+    pt.write("#This is typed by the bot!!")
+    pt.press("enter")
+    pt.write("#Another line typed by the bot!!")
+    #This is typed by the bot!!
+    #Another line typed by the bot!!
 
     # Screenshot
     # pt.screenshot(<path/>"myscreenshot.png")
 
     # Locate graphic on the screen
-    chat_bar = pt.locateOnScreen("images/B_group_icon.png", confidence = 0.8)
-    print("Target found! The position is: {0}".format(chat_bar))
-    if chat_bar is not None:
-        goto_chat_bar = pt.center(chat_bar)
-        print("Target found! The center position is: {0}".format(goto_chat_bar))
-        goto_chat_bar_X, goto_chat_bar_Y = goto_chat_bar[0]/2, goto_chat_bar[1]/2
-        # !! wait, wtf?
-        pt.click(goto_chat_bar_X, goto_chat_bar_Y, duration= 0.2)
-        pt.moveTo(100, 100, duration= 0.1)
+    # chat_bar = pt.locateOnScreen("images/B_group_icon.png", confidence = 0.8)
+    # print("Target found! The position is: {0}".format(chat_bar))
+    # if chat_bar is not None:
+    #     goto_chat_bar = pt.center(chat_bar)
+    #     print("Target found! The center position is: {0}".format(goto_chat_bar))
+    #     goto_chat_bar_X, goto_chat_bar_Y = goto_chat_bar[0]/2, goto_chat_bar[1]/2
+    #     # !! wait, wtf?
+    #     pt.click(goto_chat_bar_X, goto_chat_bar_Y, duration= 0.2)
+    #     pt.moveTo(100, 100, duration= 0.1)
 
-    else:
-        print("Target not found!")
+    # else:
+    #     print("Target not found!")
 
 
     return 0
 
 
 if __name__ == '__main__':
-    main()
+    testMinecraftMouse()
---
+++ README.md
@@ -0,0 +1,15 @@
+##############################
+
+------------------------------
+Section 1. Dependencies:
+    1) This project utilizes the library pyautogui. 
+        Make sure to install the package using the command: pip3 install pyautogui
+    
+    2) This project utilizes the library opencv-python.
+        Make sure to install the package using the command: pip3 install opencv-python
+    
+    3) This project is OS dependent.
+        Make sure to fill in the OS you are running on in the file: MiningBot_Utilities.py >> OS_TYPE = 'mac' / OS_TYPE = 'windows'
+
+    4) This project is monitor resolution dependent.
+        Please run ModuleTest.py and follow the instructions every time you run this project on a new machine. 
